590d5fa2e5754d27de55bb85165ee79f40e2adeb
Ddhruv <54676859+Ddhruv-IOT@users.noreply.github.com>
2020-06-13 14:50:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,33 @@
 <head>
 <title>SERVER TEST - AWS</title>
 </head>
+<style type="text/css">
+	body{
+		background-color: rgb(180,180,180);
+	}
+
+#a1{
+	color:blue;
+
+}
+#a2{
+	color:purple;
+	font-size: 20px;
+	font-weight: bold;
+}
+#a3{
+	color: rgb(250,50,30);
+	font-size: 20px
+
+}
+</style>
 <body>
-<p><pre>HELLO THERE!
+<h1 id = "a1">HELLO TO ALL !</h1>
+<p><pre id = "a2">
   THIS IS DDHRUV ARORA 
-  TASK-1 OF AWS-HYBRID-MULTI-CLOUD -TRAINING BY SIR VIMAL DAGA
+  TASK-1 OF AWS-HYBRID-MULTI-CLOUD-TRAINING BY SIR VIMAL DAGA
   YOU CAN CONNECT ME:
-  linkedin: https://www.linkedin.com/in/ddhruv-arora-13a868192
+  <a id="a3" href =  https://www.linkedin.com/in/ddhruv-arora-13a868192>Linkedin</a>
 </pre></p>
 <br></br>
 <pre>
